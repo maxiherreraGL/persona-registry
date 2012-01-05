d3aeefc20e8774bcd7b88b18b0439195111736b1
maxi.herrera <maxiherrera@maxiherrera-Inspiron-N4030.(none)>
2012-01-05 10:56:59 -0300

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonaRegistry::Application.config.secret_token = 'b3949e8b0ae1da797f65e05e3ca4d8fede451090d0f7bafc1debc835d90754f5a257e272d1780370ff54d24143675afd8b4c2875f770f69301f3e3a52d19d373'
