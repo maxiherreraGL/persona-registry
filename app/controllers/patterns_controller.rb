b17aa6d958cfb0223f54dd237285d187755ab35f
unknown <simon.gomez@EZE3-DNN-00450.synapse.com>
2012-01-09 17:49:45 -0300

    fix comentario

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,9 +1,12 @@
 class PatternsController < ApplicationController
   
-  /*Para llamarlo patterns#singleton */
+  #Para llamarlo patterns#singleton 
   def singleton
     singleton = Unique.instance
     otherSingleton = Unique.instance
+    
+    #El metodo new queda declarado con private en la clase Unique
+    #otherInstance = Unique.new
     
     if singleton == otherSingleton
       @message = 'true'
