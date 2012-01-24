09f2908d554c2de3aa6dad9c7e9cd7c3419d86e4
unknown <simon.gomez@EZE3-DNN-00450.synapse.com>
2012-01-24 11:50:39 -0300

    people controller

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,79 @@
+class PeopleController < ActionController::Base
+  
+  # GET /people
+  # GET /people.json
+  def index
+    @people = Person.order(:name)
+      respond_to do |format|
+      format.xml { render :xml => @people }
+    end
+  end
+
+  # GET /people/1
+  # GET /people/1.json
+  def show
+    @person = Person.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: @person }
+    end
+  end
+
+  # GET /people/new
+  # GET /people/new.json
+  def new
+    @person = Person.new
+
+    respond_to do |format|
+      format.json { render json: @person }
+    end
+  end
+
+  # GET /people/1/edit
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  # POST /people
+  # POST /people.json
+  def create
+    @person = Person.new(params[:user])
+    
+    respond_to do |format|
+      if @person.save
+        format.xml { render :xml => @person,
+        :status => :created, :location => @person }
+      else
+        format.xml { render :xml => @person.errors,
+        :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /people/1
+  # PUT /people/1.json
+  def update
+    @person = Person.find(params[:id])
+    
+    respond_to do |format|
+      if @person.update_attributes(params[:person])
+        format.xml { head :ok }
+      else
+        format.xml { render :xml => @person.errors,
+        :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /people/1
+  # DELETE /people/1.json
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+
+    respond_to do |format|
+      format.json { head :ok }
+    end
+ end
+ 
+end
